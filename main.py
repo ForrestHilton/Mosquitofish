b59f454ca47f2cad4b5fca4e39e9edeb89a14a40
Viola2112 <remycron10@gmail.com>
2023-04-05 18:35:06 -0500

    Bug fixed, displaying different models

--- main.py
+++ main.py
@@ -6,17 +6,17 @@ from graph import ordinary_plot_over_time
 # Create a new instance of the model
 
 # Run the model for 20 time steps starting with 10 juveniles and 20 adults
-#juveniles, adults = fn.model.run(SimpleModel.State(100, 100), 20)
+juveniles, adults = fn.model.run(SimpleModel.State(100, 100), 20)
 
-#ordinary_plot_over_time(juveniles, adults)
+ordinary_plot_over_time(juveniles, adults)
 
 # Now experiment with different cannibalism models
 
 # Beverton Holt
 
-#juveniles, adults = fn.bev_holt_model.run(SimpleModel.State(100, 100), 20)
+juveniles, adults = fn.bev_holt_model.run(SimpleModel.State(100, 100), 20)
 
-#ordinary_plot_over_time(juveniles, adults)
+ordinary_plot_over_time(juveniles, adults)
 
 # Allee
 
@@ -32,8 +32,8 @@ ordinary_plot_over_time(juveniles, adults)
 
 # Linear
 
-#juveniles, adults = fn.linear_model.run(SimpleModel.State(100, 100), 20)
+juveniles, adults = fn.linear_model.run(SimpleModel.State(100, 100), 20)
 
-#ordinary_plot_over_time(juveniles, adults)
+ordinary_plot_over_time(juveniles, adults)
 
 
