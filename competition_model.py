c54b61e9ef31189528fc0074a0822abf7926298f
Viola2112 <remycron10@gmail.com>
2023-04-13 18:39:32 -0500

    Now CompetitionModel inherits from Ricker

--- competition_model.py
+++ competition_model.py
@@ -4,17 +4,8 @@ import functions as fn
 """A similar class to SimpleModel except that a competition
 term is included. Ricker cannibalism is used."""
 
-
-class CompetitionModel:
-
-    # A class variable will be used to keep track of 
-    # the constant associated with the competition
-    K = 1000
-
-    class State:
-        def __init__(self, juveniles: float, adults: float):
-            self.juveniles = juveniles
-            self.adults = adults
+cm_competion_param = 1000 
+class CompetitionModel(fn.Ricker):
 
     def __init__(
         self,
@@ -22,24 +13,22 @@ class CompetitionModel:
         juvenile_survive_probability: float,
         adult_survival_probability: float,
         strength_cannibalism: float,
+        competition_param: float,
     ) -> None:
         self.fecundity = fecundity
         self.juvenile_survive_probability = juvenile_survive_probability
         self.adult_survival_probability = adult_survival_probability
         self.strength_cannibalism = strength_cannibalism
+        self.competition_param = competition_param
 
-    def description(self):
-        return type(self).__name__
-
-    def run_one_time_step(self, state: "CompetitionModel.State") -> "CompetitionModel.State":
+    def run_one_time_step(self, state: "fn.SimpleModel.State") -> "fn.SimpleModel.State":
         "takes a State object and returns a new State object representing the next time step"
         juveniles, adults = state.juveniles, state.adults
 
         births = self.fecundity * adults
         cannibalism_mult = self.cannibalism_multiplier(state)
         maturations = juveniles * self.juvenile_survive_probability * cannibalism_mult
-        # 
-        competition_deaths = state.adults*state.adults/self.K
+        competition_deaths = state.adults*state.adults/self.competition_param
 
         new_juveniles = np.maximum(births, 0)
         new_adults = np.maximum(
@@ -47,26 +36,6 @@ class CompetitionModel:
         )
 
         return CompetitionModel.State(new_juveniles, new_adults)
-
-    def run(
-        self, initial: "CompetitionModel.State", time_steps: int
-    ) -> tuple[np.ndarray, np.ndarray]:
-        "takes a initial condition and produces a tuple of numpy arrays (juveniles, adults)"
-
-        current_state = initial
-        juveniles_array = np.array([initial.juveniles])
-        adults_array = np.array([initial.adults])
-
-        for _ in range(time_steps - 1):
-            current_state = self.run_one_time_step(current_state)
-            juveniles_array = np.append(juveniles_array, current_state.juveniles)
-            adults_array = np.append(adults_array, current_state.adults)
-
-        return juveniles_array, adults_array
-
-    def cannibalism_multiplier(self, state: State):
-        # Ricker competition, as specified in the instructions
-        return np.exp(-self.strength_cannibalism*state.adults)
     
 # Object for use in main -- need to import functions for
 competition_model = CompetitionModel(
@@ -74,4 +43,5 @@ competition_model = CompetitionModel(
     fn.juvenile_survival_probability,
     fn.adult_survival_probability,
     fn.strength_can_ricker,
+    cm_competion_param,
 )
